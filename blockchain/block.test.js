a74eb7bd1cae7ba9b7c09073e20d2ae9a1785059
Jan Petersen <jpcopenhagen@protonmail.com>
2018-11-22 12:59:33 +0100

    simple DIFFICULTY

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,5 +1,5 @@
 const Block = require('./block');
-
+const { DIFFICULTY } = require('../config');
 
 describe('Block',() => {
     let data, lastBlock, block;
@@ -18,4 +18,8 @@ describe('Block',() => {
         expect(block.lastHash).toEqual(lastBlock.hash);
     });
 
+    it('generates a hash that matches the difficulty',() =>{
+        expect(block.hash.substring(0,DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+        console.info(block.toString());
+    });
 });
